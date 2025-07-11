a7fa5b339c450ff209d3a1ca963827a2669ab84c
Orlando Castaneda <ocastaneda@live.com>
2025-07-11 12:33:06 -0400

    fix docstrings, compute softmax prob before accuracy

--- deepfake_utils/train.py
+++ deepfake_utils/train.py
@@ -1,5 +1,6 @@
 import torch
 from torcheval.metrics.functional import binary_auroc, binary_auprc, binary_accuracy
+from torch.nn.functional import softmax
 
 def train_epoch(dataloader, model, loss_fn, optimizer,  device, print_progress = True):
     """
@@ -26,6 +27,8 @@ def train_epoch(dataloader, model, loss_fn, optimizer,  device, print_progress =
             Area under receiver operating characteristic on training data after 1 iteration of training
         training PR AUC: float
             Area under precision-recall curve (i.e. average precision) on training data after 1 iteration of training
+        training accuracy: float
+            Accuracy on training data after 1 iteration of training
     """
     # learn the weights
     model.train()
@@ -67,17 +70,19 @@ def validate_epoch(dataloader, model, loss_fn, device, print_progress = True):
     Returns
     ----------
         validation loss: float
-            Loss on training data after 1 iteration of training
+            Loss on passed data
         validation ROC AUC: float
-            Area under receiver operating characteristic on training data after 1 iteration of training
+            Area under receiver operating characteristic on passed data
         validation PR AUC: float
-            Area under precision-recall curve (i.e. average precision) on training data after 1 iteration of training
+            Area under precision-recall curve (i.e. average precision) on passed data
+        validation accuracy: float
+            Accuracy on passed data
     """
     model.eval()
 
     # initialize variables to compute metrics over entire dataset
     val_loss = 0
-    all_preds = torch.zeros((0,2)).to(device)
+    all_pred_prob = torch.zeros((0,2)).to(device)
     all_labels = torch.zeros(0).to(device)
 
     # to compute metrics properly, switch reduction method to 'sum' if not already
@@ -91,19 +96,21 @@ def validate_epoch(dataloader, model, loss_fn, device, print_progress = True):
             y = y.to(device)
 
             pred = model(X)
-            # append batch predictions and labels
-            all_preds = torch.cat((all_preds, pred), dim = 0)
-            all_labels = torch.cat((all_labels, y), dim = 0)
             val_loss += loss_fn(pred, y).item()
+            
+            # append batch predictions and labels
+            pred_prob = softmax(pred, dim = 1)
+            all_pred_prob = torch.cat((all_pred_prob, pred_prob), dim = 0)
+            all_labels = torch.cat((all_labels, y), dim = 0)
         
             current = batch * dataloader.batch_size + len(X)
             if print_progress:
                 print(f"Evaluation Progress: \t[{current:>5d}/{len(dataloader.dataset):>5d}]")
 
     # compute metrics over all samples
-    val_auroc = binary_auroc(all_preds[:,1], all_labels)
-    val_auprc = binary_auprc(all_preds[:,1], all_labels)
-    val_acc = binary_accuracy(all_preds[:,1], all_labels, threshold = 0.5)
+    val_auroc = binary_auroc(all_pred_prob[:,1], all_labels)
+    val_auprc = binary_auprc(all_pred_prob[:,1], all_labels)
+    val_acc = binary_accuracy(all_pred_prob[:,1], all_labels, threshold = 0.5)
     val_loss /= len(dataloader.dataset)
 
     return val_loss, val_auroc.item(), val_auprc.item(), val_acc.item()
